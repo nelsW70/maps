c11e5d6f1eef67bedecc7c50c3e8535d712729f0
Nelson Foltz <nelsfoltz@gmail.com>
2020-05-23 16:09:51 -0400

    add: interface for addMarker method

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-// import { User } from './User';
-// import { Company } from './Company';
-
-// const user = new User();
-// const company = new Company();
-
-// console.log(user);
-// console.log(company);
-
 import { CustomMap } from './CustomMap';
+import { User } from './User';
+import { Company } from './Company';
 
-const customMap = new CustomMap();
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+// customMap.addCompanyMarker(company);
