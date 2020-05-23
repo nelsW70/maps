2036b251908366172c5aedbe92f3c5bc4d3cf624
Nelson Foltz <nelsfoltz@gmail.com>
2020-05-23 14:50:27 -0400

    add: private instance via class of google map

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -117,21 +117,46 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/index.ts":[function(require,module,exports) {
-// import { User } from './User';
+})({"src/CustomMap.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.CustomMap = void 0;
+
+var CustomMap =
+/** @class */
+function () {
+  function CustomMap() {
+    this.googleMap = new google.maps.Map(document.getElementById('map'), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  return CustomMap;
+}();
+
+exports.CustomMap = CustomMap;
+},{}],"src/index.ts":[function(require,module,exports) {
+"use strict"; // import { User } from './User';
 // import { Company } from './Company';
-// const user = new User();
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+}); // const user = new User();
 // const company = new Company();
 // console.log(user);
 // console.log(company);
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
-},{}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+
+var CustomMap_1 = require("./CustomMap");
+
+var customMap = new CustomMap_1.CustomMap();
+},{"./CustomMap":"src/CustomMap.ts"}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,6 @@
 // console.log(user);
 // console.log(company);
 
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+import { CustomMap } from './CustomMap';
+
+const customMap = new CustomMap();
